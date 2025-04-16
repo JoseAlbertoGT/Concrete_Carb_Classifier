95c276a5c7e6103dfc89903c7576e74b83425398
José Alberto Guzmán Torres <179648139+JoseAlbertoGT@users.noreply.github.com>
2025-04-16 17:25:03 -0600

    Update Central_app.py

--- Central_app.py
+++ Central_app.py
@@ -47,15 +47,15 @@ def run_classifier(img):
 set_page_configuration()
 background("Images/side_bar_img.png")
 
-# Hidding the hamburger button
-hide_streamlit_style = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-header {visibility:hidden;}
-</style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+# # Hidding the hamburger button
+# hide_streamlit_style = """
+# <style>
+# #MainMenu {visibility: hidden;}
+# footer {visibility: hidden;}
+# header {visibility:hidden;}
+# </style>
+# """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Hidding the Github button
 hide_github_icon = """
